f5851107fb66fb223b68816b7cd101b50fc17054
egthiago <thiago.eg61@gmail.com>
2026-05-03 17:58:19 -0300

    Remove input for first and second lists
    Removed input prompts for the first and second lists.

--- exercicio_4.py
+++ exercicio_4.py
@@ -35,9 +35,6 @@ segunda_lista = [*map(int, input("Digite a sua segunda lista (separando os núme
 
 # Fazer a partir daqui
 
-primeira_lista = [*map(int, input("Digite a sua primeira lista (separando os números por vírgula): ").split(","))]
-segunda_lista = [*map(int, input("Digite a sua segunda lista (separando os números por vírgula): ").split(","))]
-
 maior_primeira = 0
 maior_segunda = 0
 
